a7406a4db8d2e1652d52f08779269cb1dfb0ea7b
hlord2000 <kellyhlord@gmail.com>
2022-01-19 23:13:00 -0500

    :

--- Firmware/code.py
+++ Firmware/code.py
@@ -9,7 +9,7 @@ from adafruit_hid.keycode import Keycode
 from adafruit_mcp230xx.mcp23017 import MCP23017
 
 # To use default I2C bus (most boards)
-i2c = busio.I2C(board.A2,board.A3)
+i2c = busio.I2C(board.A2,board.A3,frequency=400000)
 
 mcp = [MCP23017(i2c, address = expander) for expander in [0x22, 0x20, 0x21]]
 
@@ -28,13 +28,13 @@ layers = [
 [
 Keycode.ESCAPE, Keycode.Q, Keycode.W, Keycode.E, Keycode.R, Keycode.T, Keycode.Y, Keycode.U, Keycode.I, Keycode.O, Keycode.P, Keycode.BACKSPACE,
 Keycode.TAB, Keycode.A, Keycode.S, Keycode.D, Keycode.F, Keycode.G, Keycode.H, Keycode.J, Keycode.K, Keycode.L, Keycode.SEMICOLON, Keycode.ENTER,
-Keycode.SHIFT, Keycode.Z, Keycode.X, Keycode.C, Keycode.V, Keycode.B, Keycode.N, Keycode.M, Keycode.COMMA, Keycode.PERIOD, Keycode.FORWARD_SLASH, Keycode.SHIFT,
+Keycode.SHIFT, Keycode.Z, Keycode.X, Keycode.C, Keycode.V, Keycode.B, Keycode.N, Keycode.M, Keycode.COMMA, Keycode.PERIOD, Keycode.FORWARD_SLASH, Keycode.QUOTE,
 Keycode.LEFT_CONTROL, Keycode.LEFT_GUI, Keycode.LEFT_ALT, Keycode.F20, "DOWN", Keycode.SPACE, "UP", Keycode.APPLICATION, Keycode.RIGHT_GUI, Keycode.RIGHT_ALT, Keycode.RIGHT_CONTROL
 ],
 
 [
 Keycode.ESCAPE, Keycode.ONE, Keycode.TWO, Keycode.THREE, Keycode.FOUR, Keycode.FIVE, Keycode.SIX, Keycode.SEVEN, Keycode.EIGHT, Keycode.NINE, Keycode.ZERO, Keycode.BACKSPACE,
-Keycode.TAB, Keycode.MINUS, Keycode.EQUALS, Keycode.LEFT_BRACKET, Keycode.RIGHT_BRACKET, Keycode.G, Keycode.LEFT_ARROW, Keycode.DOWN_ARROW, Keycode.UP_ARROW, Keycode.RIGHT_ARROW, Keycode.SEMICOLON, Keycode.ENTER,
+Keycode.TAB, Keycode.MINUS, Keycode.EQUALS, Keycode.LEFT_BRACKET, Keycode.RIGHT_BRACKET, Keycode.G, Keycode.LEFT_ARROW, Keycode.DOWN_ARROW, Keycode.UP_ARROW, Keycode.RIGHT_ARROW, Keycode.SEMICOLON, Keycode.QUOTE,
 Keycode.SHIFT, Keycode.Z, Keycode.X, Keycode.C, Keycode.V, Keycode.B, Keycode.N, Keycode.M, Keycode.COMMA, Keycode.PERIOD, Keycode.FORWARD_SLASH, Keycode.SHIFT,
 Keycode.LEFT_CONTROL, Keycode.LEFT_GUI, Keycode.LEFT_ALT, Keycode.F20, "DOWN", Keycode.SPACE, "UP", Keycode.APPLICATION, Keycode.RIGHT_GUI, Keycode.RIGHT_ALT, Keycode.RIGHT_CONTROL
 ],
@@ -42,7 +42,7 @@ Keycode.LEFT_CONTROL, Keycode.LEFT_GUI, Keycode.LEFT_ALT, Keycode.F20, "DOWN", K
 [
 Keycode.F1, Keycode.F2, Keycode.F3, Keycode.F4, Keycode.F5, Keycode.F6, Keycode.F7, Keycode.F8, Keycode.F9, Keycode.F10, Keycode.F11, Keycode.F12,
 Keycode.TAB, Keycode.A, Keycode.S, Keycode.D, Keycode.F, Keycode.G, Keycode.H, Keycode.J, Keycode.K, Keycode.L, Keycode.SEMICOLON, Keycode.ENTER,
-Keycode.SHIFT, Keycode.Z, Keycode.X, Keycode.C, Keycode.V, Keycode.B, Keycode.N, Keycode.M, Keycode.COMMA, Keycode.PERIOD, Keycode.FORWARD_SLASH, Keycode.SHIFT,
+Keycode.SHIFT, Keycode.Z, Keycode.X, Keycode.C, Keycode.V, Keycode.B, Keycode.N, Keycode.M, Keycode.COMMA, Keycode.PERIOD, Keycode.FORWARD_SLASH, Keycode.QUOTE,
 Keycode.LEFT_CONTROL, Keycode.LEFT_GUI, Keycode.LEFT_ALT, Keycode.F20, "DOWN", Keycode.SPACE, "UP", Keycode.APPLICATION, Keycode.RIGHT_GUI, Keycode.RIGHT_ALT, Keycode.RIGHT_CONTROL
 ]
 
@@ -111,57 +111,43 @@ matrix_converted = [
         
 key_state = [0,0,0]
 prev_state = [0,0,0]
-to_be_pressed = []
-to_be_released = []
+held = []
 layer_index = 0
-up_not_pressed = True
-down_not_pressed = True
+
 while True:
     try:
         for i, expander in enumerate(mcp):    
-            key_state[i-1] = bin((expander.gpiob << 8) | expander.gpioa)
-        #print(key_state, "\n", prev_state, "\n")
+            key_state[i-1] = 65535 - (((expander.gpiob) << 8) | (expander.gpioa))
+        
         for i in range(0,3):
             for j in range(0,16):
-                key = keeboord_matrix[matrix_converted[i][j]-1]
-                if all([~(int(key_state[i]) >> (j)) & 1, key not in to_be_pressed]):
-                    if all([key == "UP", up_not_pressed]):
-                        if layer_index + 1 > len(layers) - 1:
-                            layer_index = 0
-                        else:
-                            layer_index = layer_index + 1
-                        keeboord_matrix = layers[layer_index]
-                        up_not_pressed = False
+                key_code = keeboord_matrix[matrix_converted[i][j]-1]
+                key_pressed = int((key_state[i]) >> (j)) & 1
+                
+                if all([key_pressed, key_code == "UP", key_code not in held]):
+                    if layer_index + 1 > 2:
+                        layer_index = 0
+                    else:
+                        layer_index = layer_index + 1
+                    keeboord_matrix = layers[layer_index]
+                    held.append(key_code)
+                    continue
+                elif all([key_pressed, key_code == "DOWN", key_code not in held]):
+                    if layer_index - 1 < 0:
+                        layer_index = 2
+                    else:
+                        layer_index = layer_index - 1
+                    keeboord_matrix = layers[layer_index]
+                    held.append(key_code)
+                    continue
+
+                if all([key_pressed, key_code not in held]):
+                    keyboard.press(key_code)
+                    held.append(key_code)
+                elif all([not key_pressed, key_code in held]):
+                    held.remove(key_code)
+                    if any([key_code == "UP", key_code == "DOWN"]):
                         continue
-                    elif all([key == "DOWN", down_not_pressed]):
-                        if layer_index - 1 < 0:
-                           layer_index = len(layers) - 1
-                        else:
-                            layer_index = layer_index - 1
-                        keeboord_matrix = layers[layer_index]
-                        down_not_pressed = False
-                        continue    
-                    elif any([key == "UP", key == "DOWN"]):
-                        continue
-                    to_be_pressed.append(key)
-                elif all([key in to_be_pressed, key not in to_be_released]):
-                    keyboard.press(*to_be_pressed, )
-                    to_be_released.append(key)
-                elif all([key in to_be_released, not ~(int(key_state[i]) >> (j)) & 1]):
-                    keyboard.release(*to_be_released, )
-                    to_be_pressed.remove(key)
-                    to_be_released.remove(key)
-                elif all([not ~(int(key_state[i]) >> (j)) & 1, key == "UP"]):
-                    up_not_pressed = True
-                elif all([not ~(int(key_state[i]) >> (j)) & 1, key == "DOWN"]):
-                    down_not_pressed = True
+                    keyboard.release(key_code)
     except ValueError:
-        to_be_pressed = []
-        to_be_released = []
-        keyboard.release_all()
-        print("fucky wucky")
         pass
-    
-    
-    
-    
